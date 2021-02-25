105ee3123b7bfeed854cb919b9f5da6cb4057d62
ehanhan <78825648+ehanhan@users.noreply.github.com>
2021-02-25 07:42:55 -0800

    Create AddressBookApplication.java

---
+++ AddressBookApplication.java
@@ -0,0 +1,19 @@
+/**
+ * @author ehabh
+ *
+ * This is the Main class of the Address Book program
+ * It first Creates an instance of an Address Book object
+ * then creates an instance of a Menu object
+ * finally it passes the addressbook object into the Menu's Run method,
+ * which initiates the User interface menu
+ *
+ */
+public class AddressBookApplication {
+
+    public static void main(String args[]) {
+
+        AddressBook ab = new AddressBook();
+        Menu menu = new Menu();
+        Menu.Run(ab);
+    }
+}
